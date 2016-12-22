f59a488e07201ac0f60b5e6b641857956152d0c9
Kirill Bulatov <kmbulatov@yamoney.ru>
2016-12-22 17:57:03 +0300

    Сделал главный плагин более читаемым, поправил способ применения плагинов.

--- src/main/java/ru/yandex/money/gradle/plugins/library/LibraryProjectPlugin.java
+++ src/main/java/ru/yandex/money/gradle/plugins/library/LibraryProjectPlugin.java
@@ -7,6 +7,7 @@ import ru.yandex.money.gradle.plugins.library.readme.ReadmePlugin;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.stream.Stream;
 
 /**
  * Входная точка library-plugin'а, подключает все необходимые плагины-зависимости.
@@ -20,30 +21,30 @@ public class LibraryProjectPlugin implements Plugin<Project> {
      * Для подключения новой функциональности, достаточно добавить плагин в этот список.
      * Все остальные настройки должны делаться в самом добавляемом плагине.
      */
-    private static final Collection<Plugin<Project>> PLUGINS_TO_APPLY = Arrays.asList(
-            new ReadmePlugin()
+    private static final Collection<Class<?>> PLUGINS_TO_APPLY = Arrays.asList(
+            ReadmePlugin.class
     );
 
     @Override
     public void apply(Project project) {
-        PLUGINS_TO_APPLY.forEach(plugin -> plugin.apply(project));
+        PLUGINS_TO_APPLY.forEach(pluginClass -> project.getPluginManager().apply(pluginClass));
         configureRepositories(project);
     }
 
-    /** Добавляем в проект все репозитории, нужные для получения зависимостей. */
+    /**
+     * Добавляем в проект все репозитории, нужные для получения зависимостей.
+     */
     private void configureRepositories(Project project) {
         RepositoryHandler repositories = project.getRepositories();
-        repositories.add(repositories.maven(mavenArtifactRepository ->
-                mavenArtifactRepository.setUrl("http://nexus.yamoney.ru/content/repositories/central/")));
-        repositories.add(repositories.maven(mavenArtifactRepository ->
-                mavenArtifactRepository.setUrl("http://nexus.yamoney.ru/content/repositories/releases/")));
-        repositories.add(repositories.maven(mavenArtifactRepository ->
-                mavenArtifactRepository.setUrl("http://nexus.yamoney.ru/content/repositories/snapshots/")));
-        repositories.add(repositories.maven(mavenArtifactRepository ->
-                mavenArtifactRepository.setUrl("http://nexus.yamoney.ru/content/repositories/thirdparty/")));
-        repositories.add(repositories.maven(mavenArtifactRepository ->
-                mavenArtifactRepository.setUrl("http://nexus.yamoney.ru/content/repositories/spp-snapshots/")));
-        repositories.add(repositories.maven(mavenArtifactRepository ->
-                mavenArtifactRepository.setUrl("http://nexus.yamoney.ru/content/repositories/spp-releases/")));
+        Stream.of(
+                "http://nexus.yamoney.ru/content/repositories/central/",
+                "http://nexus.yamoney.ru/content/repositories/releases/",
+                "http://nexus.yamoney.ru/content/repositories/snapshots/",
+                "http://nexus.yamoney.ru/content/repositories/thirdparty/",
+                "http://nexus.yamoney.ru/content/repositories/spp-snapshots/",
+                "http://nexus.yamoney.ru/content/repositories/spp-releases/"
+        )
+                .map(repoUrl -> repositories.maven(mavenArtifactRepository -> mavenArtifactRepository.setUrl(repoUrl)))
+                .forEach(repositories::add);
     }
 }
